eadf5a15ef62c30ecca9995da2bb00eee46de839
unknown <valentinarengifonarvaez1111mail.com>
2023-09-20 14:40:05 -0500

    LOGIN

---
+++ app/src/main/java/com/ingelist/moderndashboard/ClothingActivity.kt
@@ -0,0 +1,11 @@
+package com.ingelist.moderndashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClothingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clothing)
+    }
+}
